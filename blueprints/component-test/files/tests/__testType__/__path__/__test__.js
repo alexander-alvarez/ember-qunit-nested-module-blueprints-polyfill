ae866b47c174e64a95ce4647ad56cd88336717d2
Alex Alvarez <alexander-alvarez@github.com>
2017-12-07 22:09:31 -0500

    Setup test-runner & component-test blueprint
    Shamelessly stolen from implementation in https://github.com/emberjs/ember.js/pull/15934

--- blueprints/component-test/files/tests/__testType__/__path__/__test__.js
+++ blueprints/component-test/files/tests/__testType__/__path__/__test__.js
@@ -1,30 +1,37 @@
-import { module, test } from 'qunit';
-import { <% if (testType === 'integration' ) { %> setupRenderingTest <% } else if(testType === 'unit') { %>  setupTest <% } %> } from 'ember-qunit';
+<% if (testType === 'integration') { %>import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
 module('<%= friendlyTestDescription %>', function(hooks) {
-<% if (testType === 'integration' ) { %>  setupRenderingTest(hooks); <% } else if(testType === 'unit') { %>  setupTest(hooks); <% } %>
+  setupRenderingTest(hooks);
 
-  // use hooks.before/after/beforeEach/afterEach for test setup
-
-  test('it renders', async function(assert) {<% if (testType === 'integration' ) { %>
+  test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
+    // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await this.render(hbs`{{<%= componentPathName %>}}`);
+    await render(hbs`{{<%= componentPathName %>}}`);
 
-    assert.equal(this.$().text().trim(), '');
+    assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
-    await this.render(hbs`
+    await render(hbs`
       {{#<%= componentPathName %>}}
         template block text
       {{/<%= componentPathName %>}}
     `);
 
-    assert.equal(this.$().text().trim(), 'template block text');<% } else if(testType === 'unit') { %>
-    // Creates the component instance
-    let Factory = this.owner.factoryFor('component:<%= componentPathName %>');
-    let subject = Factory.create({});
-    assert.ok(subject);<% } %>
+    assert.equal(this.element.textContent.trim(), 'template block text');
   });
-});
+});<% } else if (testType === 'unit') { %>import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('<%= friendlyTestDescription %>', function(hooks) {
+  setupTest(hooks);
+
+  test('it exists', function(assert) {
+    let component = this.owner.factoryFor('component:<%= componentPathName %>').create();
+    assert.ok(component);
+  });
+});<% } %>
+
---
+++ node-tests/blueprints/component-test-test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+const setupTestHooks = blueprintHelpers.setupTestHooks;
+const emberNew = blueprintHelpers.emberNew;
+const emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+const modifyPackages = blueprintHelpers.modifyPackages;
+
+const chai = require('ember-cli-blueprint-test-helpers/chai');
+const expect = chai.expect;
+
+const fixture = require('../helpers/fixture');
+
+describe('Blueprint: component-test', function() {
+  setupTestHooks(this);
+
+  describe('in app', function() {
+    beforeEach(function() {
+      return emberNew();
+    });
+
+    describe('with ember-cli-qunit@4.1.1', function() {
+
+      it('component-test x-foo', function() {
+        return emberGenerateDestroy(['component-test', 'x-foo'], _file => {
+          expect(_file('tests/integration/components/x-foo-test.js'))
+          .to.equal(fixture('component-test/rfc232.js'));
+        });
+      });
+
+      it('component-test x-foo --unit', function() {
+        return emberGenerateDestroy(['component-test', 'x-foo', '--unit'], _file => {
+          expect(_file('tests/unit/components/x-foo-test.js'))
+          .to.equal(fixture('component-test/rfc232-unit.js'));
+        });
+      });
+    });
+
+  });
+});
