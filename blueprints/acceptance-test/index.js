5b47a3721adf886e50c6b7524d514a0ef0d9baf2
Robert Jackson <rjackson@linkedin.com>
2017-12-17 13:01:55 -0500

    Add acceptance-test blueprint.

---
+++ blueprints/acceptance-test/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* eslint-env node */
+
+const path = require('path');
+const testInfo = require('ember-cli-test-info');
+const pathUtil = require('ember-cli-path-utils');
+const stringUtils = require('ember-cli-string-utils');
+const existsSync = require('exists-sync');
+
+module.exports = {
+  description: 'Generates an acceptance test for a feature.',
+
+  locals: function(options) {
+    let testFolderRoot = stringUtils.dasherize(options.project.name());
+
+    if (options.project.isEmberCLIAddon()) {
+      testFolderRoot = pathUtil.getRelativeParentPath(options.entity.name, -1, false);
+    }
+
+    let destroyAppExists =
+      existsSync(path.join(this.project.root, '/tests/helpers/destroy-app.js'));
+
+    return {
+      testFolderRoot: testFolderRoot,
+      friendlyTestName: testInfo.name(options.entity.name, 'Acceptance', null),
+      destroyAppExists: destroyAppExists
+    };
+  }
+};
