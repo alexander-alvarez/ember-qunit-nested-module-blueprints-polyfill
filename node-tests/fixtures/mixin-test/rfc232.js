30b0c9bc06ae08bbc4481702044af0a195b6fd07
Ben Demboski <ben@turbopatent.com>
2018-02-23 14:37:16 -0800

    Fix spacing error in mixin test (#12)
    Remove the space before the function paren to match the style of all the other test blueprints

--- node-tests/fixtures/mixin-test/rfc232.js
+++ node-tests/fixtures/mixin-test/rfc232.js
@@ -4,7 +4,7 @@ import { module, test } from 'qunit';
 
 module('Unit | Mixin | foo', function() {
   // Replace this with your real tests.
-  test('it works', function (assert) {
+  test('it works', function(assert) {
     let FooObject = EmberObject.extend(FooMixin);
     let subject = FooObject.create();
     assert.ok(subject);
