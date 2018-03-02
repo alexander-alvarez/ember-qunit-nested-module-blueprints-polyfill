3f165c3aa543a4ea8aa8681ccb68c937784a6f71
Alex Alvarez <alexander-alvarez@github.com>
2018-03-01 23:11:47 -0500

    use ui on the Addon instance

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 /* eslint-env node */
 'use strict';
 let VersionChecker = require('ember-cli-version-checker');
-const UI = require('console-ui');
-const ui = new UI({
-  inputStream: process.stdin,
-  outputStream: process.stdout,
-  errorStream: process.stderr,
-  writeLevel: 'INFO',
-  ci: true
-});
-
 
 module.exports = {
   name: 'ember-qunit-nested-module-blueprints-polyfill',
@@ -18,10 +9,10 @@ module.exports = {
     let dep = checker.forEmber();
     let deprecated = dep.gte('3.0.0');
     if (deprecated) {
-      ui.writeError('ember-qunit-nested-module-blueprints-polyfill is no longer needed in Ember 3.0. ');
-      ui.writeError('Please remove ember-qunit-nested-module-blueprints-polyfill from your package.json.');
-      ui.writeError('See the following for more info:\n');
-      ui.writeError('https://github.com/alexander-alvarez/ember-qunit-nested-module-blueprints-polyfill/issues/13');
+      this.ui.writeError('ember-qunit-nested-module-blueprints-polyfill is no longer needed in Ember 3.0. ');
+      this.ui.writeError('Please remove ember-qunit-nested-module-blueprints-polyfill from your package.json.');
+      this.ui.writeError('See the following for more info:\n');
+      this.ui.writeError('https://github.com/alexander-alvarez/ember-qunit-nested-module-blueprints-polyfill/issues/13');
     }
     return !deprecated;
   }
